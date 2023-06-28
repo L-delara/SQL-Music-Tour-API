a30bb8ba9ebf2c3ebd93f392f99d2f49640cf51d
L-delara <ligia.delara15@gmail.com>
2023-06-27 20:39:36 -0400

    minor fix

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 // SEQUELIZE CONNECTION
 const sequelize = new Sequelize(process.env.PG_URI);
 
+try {
+  sequelize.authenticate();
+  console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
+} catch (err) {
+  console.log(`Unable to connect to PG: ${err}`);
+}
 // ROOT
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -22,10 +28,3 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
 });
-
-try {
-  sequelize.authenticate();
-  console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
-} catch (err) {
-  console.log(`Unable to connect to PG: ${err}`);
-}
